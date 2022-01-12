d2240dce1d7c5408f53e63aa0a34b4a85bccb4b0
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-12 17:34:12 -0300

    reset local storaged if cart change

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,4 +1,11 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { toast } from "react-toastify";
 import { api } from "../services/api";
 import { Product, Stock } from "../types";
@@ -33,6 +40,20 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     return [];
   });
 
+  const prevCartRef = useRef<Product[]>(cart);
+
+  useEffect(() => {
+    prevCartRef.current = cart;
+  });
+
+  const cartPreviousValue = prevCartRef.current ?? cart;
+
+  useEffect(() => {
+    if (cart !== cartPreviousValue) {
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(cart));
+    }
+  }, [cart, cartPreviousValue]);
+
   const addProduct = async (productId: number) => {
     try {
       const updatedCart = [...cart];
@@ -59,7 +80,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       }
 
       setCart(updatedCart);
-      localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCart));
     } catch {
       toast.error("Erro na adição do produto");
     }
@@ -74,10 +94,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       if (productIndex >= 0) {
         productsInCart.splice(productIndex, 1);
         setCart(productsInCart);
-        localStorage.setItem(
-          "@RocketShoes:cart",
-          JSON.stringify(productsInCart)
-        );
       } else {
         throw Error();
       }
@@ -105,10 +121,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       if (productIndex) {
         productIndex.amount = amount;
         setCart(productsInCart);
-        localStorage.setItem(
-          "@RocketShoes:cart",
-          JSON.stringify(productsInCart)
-        );
       } else {
         throw Error();
       }
